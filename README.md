547ee0a612ab06fc927eba7bb6b30e34bb00b166
Ronald Kwan <rkwan@berkeley.edu>
2016-02-11 02:03:16 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Math Tournament Forms
+
+## Prerequisites
+
+Python 2.7, Flask.
+
+## Instructions
+
+Run `python run.py`, and go to `localhost:5000`. Probably requires fiddling with the margins when printing.
+
+## Credits
+
+Adapted from Charles Chen's code.
---
+++ run.py
@@ -0,0 +1,20 @@
+import math
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def signupsheets():
+    start = 1
+    end = 99
+    team_size = 8
+    event_name = "A-Star Math Tournament"
+
+    id_len = int(math.floor(math.log10(end)+1))
+    id_seq = [str(n).zfill(id_len) for n in range(start, end+1)]
+    letter_seq = [chr(ord("A") - 1 + i) for i in range(1, team_size+1)]
+    return render_template("signupsheets.html",
+        event_name=event_name, id_seq=id_seq, letter_seq=letter_seq, last_letter=letter_seq[-1])
+
+if __name__ == '__main__':
+    app.run()
